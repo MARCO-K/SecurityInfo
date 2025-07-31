e1bbaf4f298926b3797f8dc0240ff4aaed0fa5e2
Marco Kleinert <marco.kleinert@outlook.com>
2025-07-31 09:34:50 +0200

    Add initial Pester tests for Get-CisaKev function

---
+++ tests/Get-CisaKev.Tests.ps1
@@ -0,0 +1,8 @@
+# Tests for Get-CisaKev function
+# TODO: Add Pester tests for the Get-CisaKev function.
+
+Describe 'Get-CisaKev' {
+    It 'Should be tested' {
+        $true | Should -Be $true
+    }
+}
